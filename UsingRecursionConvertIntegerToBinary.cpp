2228d0f3f9ea6f518a5676168c8eafd4ca497f92
KirillEAgaev <161639846+KirillEAgaev@users.noreply.github.com>
2024-07-11 22:26:46 +0300

    Lab3 Exercise 5

---
+++ UsingRecursionConvertIntegerToBinary.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+// Рекурсивная функция для перевода числа из десятичной в двоичную систему
+void decimalToBinary(int num) {
+    if (num > 0) {
+        decimalToBinary(num / 2);
+        cout << num % 2;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int num;
+    cout << "Введите целое положительное число для перевода в двоичную систему: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "Двоичное представление числа " << num << " : ";
+        decimalToBinary(num);
+        cout << endl;
+    }
+    else {
+        cout << "Введите целое положительное число." << endl;
+    }
+
+    return 0;
+}
