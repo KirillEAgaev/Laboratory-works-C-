953311a4c6bd18d022fd919602f9f1e8b3e5ae4e
KirillEAgaev <161639846+KirillEAgaev@users.noreply.github.com>
2024-07-11 22:23:29 +0300

    Lab3 Exercise 2

---
+++ ApplyingFunctionImplementationIteration.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Функция вычисления кубического корня с использованием стандартной функции pow
+double cubeRootPow(double a) {
+    return pow(a, 1.0 / 3.0);
+}
+
+// Функция вычисления кубического корня с использованием итерационной формулы
+double cubeRootIterative(double a) {
+    double xi = 1.0;
+    double prevXi;
+
+    do {
+        prevXi = xi;
+        xi = 1.0 / 3.0 * (a / (prevXi * prevXi) + 2 * prevXi);
+    } while (abs(xi - prevXi) > 1e-6); // Проверка на достижение заданной точности
+
+    return xi;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    double a;
+
+    cout << "Введите число a для вычисления кубического корня: ";
+    cin >> a;
+
+    if (a >= 0) {
+        cout << "Кубический корень числа a (pow): " << cubeRootPow(a) << endl;
+        cout << "Кубический корень числа a (итерация): " << cubeRootIterative(a) << endl;
+    }
+    else {
+        cout << "Кубический корень определен только для неотрицательных чисел." << endl;
+    }
+
+    return 0;
+}
