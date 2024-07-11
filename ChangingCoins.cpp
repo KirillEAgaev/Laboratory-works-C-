a069da063be4f071061771682087b3f9991cf006
KirillEAgaev <161639846+KirillEAgaev@users.noreply.github.com>
2024-07-11 22:20:28 +0300

    Lab2 Exercise 3

---
+++ ChangingCoins.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Задаем номиналы монет
+    vector<int> coins = { 10, 5, 2, 1 };
+
+    int amount;
+    cout << "Введите сумму для размена: ";
+    cin >> amount;
+
+    cout << "Минимальный набор монет для размена " << amount << " рублей:\n";
+
+    for (int i = 0; i < coins.size(); i++) {
+        int count = amount / coins[i];
+        amount -= count * coins[i];
+        if (count > 0) {
+            cout << count << " монет(ы) по " << coins[i] << " рублей\n";
+        }
+    }
+
+    return 0;
+}
