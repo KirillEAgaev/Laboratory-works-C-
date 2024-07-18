315da6a61bf79b2756adbc2344ce8eb133977564
KirillEAgaev <161639846+KirillEAgaev@users.noreply.github.com>
2024-07-18 22:47:32 +0300

    Lab9 Exercise 1

---
+++ SafeImplementationOfTriangleClass.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class TriangleException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Невозможно создать треугольник с такими сторонами";
+    }
+};
+
+class Triangle {
+private:
+    double a, b, c;
+
+public:
+    Triangle(double a, double b, double c) {
+        if (a + b <= c || b + c <= a || a + c <= b) {
+            throw TriangleException();
+        }
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+
+    double area() const {
+        double p = (a + b + c) / 2;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+};
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+
+    try {
+        Triangle t(3, 4, 5);
+        cout << "Площадь треугольника: " << t.area() << endl;
+
+        // Попытка создать треугольник с недопустимыми сторонами
+        Triangle t2(3, 4, 10);
+    }
+    catch (const TriangleException& e) {
+        cerr << "Ошибка: " << e.what() << endl;
+    }
+    return 0;
+}
