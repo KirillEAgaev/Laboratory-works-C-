bdf89a627d19e4306fce5d4655b771bc16c00f20
KirillEAgaev <161639846+KirillEAgaev@users.noreply.github.com>
2024-07-21 11:55:00 +0300

    Lab14 Exercise 2

---
+++ TemplateFunctionrOutputtingDataToStream.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+void Print(const T& container, const std::string& separator) {
+    bool first = true;
+    for (const auto& element : container) {
+        if (!first) {
+            cout << separator;
+        }
+        cout << element;
+        first = false;
+    }
+    cout << "\n";
+}
+
+int main() {
+    vector<int> data = { 1, 2, 3 };
+    Print(data, ", "); // на экране: 1, 2, 3
+    return 0;
+}
